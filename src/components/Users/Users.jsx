ed0976e0bbe54eb3e0d6ea6880c65e64e6b5a970
shohag0007 <merinedrive007@gmail.com>
2024-03-19 22:53:33 +0600

    react router details

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,10 +1,21 @@
+import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
+import './Users.css';
 
 
 const Users = () => {
+    const users = useLoaderData();
+    console.log(users);
     return (
         <div>
-            <h2>Our Users</h2>
+            <h2>Our Users: {users.length}</h2>
             <p>Fantastic And Gentle Users</p>
+        <div className="users">
+            {
+                users.map(user => <User key={user.id} user={user}></User>)
+            }
+        </div>
+
         </div>
     );
 };
