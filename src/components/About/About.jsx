71e5f2d9c2bf3311ac378c61577776df153a434f
shohag0007 <merinedrive007@gmail.com>
2024-03-19 20:18:51 +0600

    react intoduction

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>Everything You Want to Know</p>
+        </div>
+    );
+};
+
+export default About;
